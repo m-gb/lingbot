03a5c078b1349a5dca82c5f9cd72e21eda0e2831
m-gb <mayu666@gmail.com>
2017-09-18 22:43:08 +0200

    lingbot test for heroku

--- lingbot.rb
+++ lingbot.rb
@@ -1,3 +1,27 @@
+require 'sinatra'
+require 'httparty'
+require 'json'
+
+post '/gateway' do
+  return if params[:token] != ENV['9XPhGp3iHr83vTIABx2Ifipp']
+  message = params[:text].gsub(params[:lingbot], '').strip
+  
+  action, repo = message.split('_').map {|c| c.strip.downcase }
+  repo_url = "https://api.github.com/repos/#{repo}"
+
+  case action
+    when 'issues'
+      resp = HTTParty.get(repo_url)
+      resp = JSON.parse resp.body
+      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+  end
+end
+
+def respond_message message
+  content_type :json
+  {:text => message}.to_json
+end
+
 module Colorized
   def colorize(str:, color_code:)
     "\e[#{color_code}m#{str}\e[0m"  
